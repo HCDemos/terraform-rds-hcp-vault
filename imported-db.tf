a7bf3c9e168152f7ff8a8f5387be7459ad34323e
peada04 <dpeacock@hashicorp.com>
2026-09-09 08:58:56 -0400

    Create imported-db.tf

---
+++ imported-db.tf
@@ -0,0 +1,16 @@
+import {
+  to       = aws_db_instance.dap_rds_1
+  provider = aws
+  identity = {
+    account_id = "188978421156"
+    identifier = "dap-rds-1"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_db_instance" "dap_rds_1" { 
+  identifier = "dap-rds-1" 
+ 
+  # Configuration populated from the 
+  # existing AWS DB instance... 
+} 
